845a357305458b5160ba1570ca7cb484ae00919f
Himanshuba9hel <himanshuba9hel@gmail.com>
2023-09-22 19:49:00 +0530

    addmoresong

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-// Import the filesystem module
-const fs = require("fs");
-
-let directory_name = "music_list";
-
-// Function to get current filenames
-// in directory
-let filenames = fs.readdirSync(directory_name);
-
-console.log("\nFilenames in directory:");
-console.log(filenames);
-
-filenames.forEach((file) => {
-	console.log("File:", file);
-});
+function change_song(name){
+    name = "/music_list/"+name;
+    document.getElementById("music_player").src = name;
+    document.body.style.backgroundColor ="red"
+}
