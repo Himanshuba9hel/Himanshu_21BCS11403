1bcb758a7cf60bd3123a1fe5d694bb3d6276bfa4
Himanshuba9hel <himanshuba9hel@gmail.com>
2023-09-22 22:28:16 +0530

    minus list

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ function change_song(name){
     name = "/music_list/"+name;
     document.getElementById("music_player").src = name;
 }
+
+var a = document.getElementById("music_player");
+function playaudio() {
+    a.play();
+}
+function pauseaudio() {
+    a.pause();
+}
+function stopaudio() {
+    a.pause();
+    a.currentTime = 0;
+}
