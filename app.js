07b684a7c41ac39f9063f5d51b898db7572bed80
Himanshuba9hel <himanshuba9hel@gmail.com>
2023-09-26 11:45:39 +0530

    new

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+var t = true;
 function change_song(name ,img){
     document.getElementById("music_player").src = name;
+    //document.getElementById("music_player").autoplay = true;
     document.getElementById("music_image").src = img;
+    const att = document.createAttribute("allow");
+    att.value = "autoplay";
+    const mp = document.getElementById("music_player");
+    mp.setAttributeNode(att);
+       
+    
+
+}
+function start_audio(){
 }
 
