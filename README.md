00551116aa672c4ba59ea4f53d6be8d6118c5a03
Himanshuba9hel <himanshuba9hel@gmail.com>
2023-09-26 11:51:55 +0530

    zero

--- README.md
+++
@@ -1,2 +0,0 @@
-# Himanshu_21BCS11403 7300319552
-Use this link to directly broadcast https://himanshuba9hel.github.io/Himanshu_21BCS11403/
--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-function change_song(name ,img){
-    document.getElementById("music_player").src = name;
-    document.getElementById("music_image").src = img;
-}
+document.getElementById("").innerHTML ="Home";
 
+
+
+
+
+
+
+// const {Level} = require('level');
+
+// const db = new Level('db',{valueEncoding: 'json'}); 
+
+// // db.put('Movie' , 'Avatar' , function(err){
+// //         console.log("you put record in");
+// //     }
+// // )
+// db.get('Movie',function(err,Movie){
+//     console.log(Movie);
+// })
+
+// // db.put('Movie','Tiger', function(err){
+// //     console.log("Inseted item");
+// // })
